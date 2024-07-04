1f48145e0a829a6b2fc01907a029c769338c5ab3
MAITRAEE MITRA <maitraee131@gmail.com>
2024-07-04 09:30:49 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-export default (phase, { defaultConfig }) => {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+ 
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return  {
  @type {import('next').NextConfig} 
 
 const nextConfig = {
