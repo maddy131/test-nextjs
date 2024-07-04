5bbdaa7ab74640ac61829f11a214210dd44348b0
MAITRAEE MITRA <maitraee131@gmail.com>
2024-07-04 10:08:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 
  /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-};
+const nextConfig = {};
 
-export default nextConfig;
+module.exports = nextConfig;
