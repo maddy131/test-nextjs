1b9b2320cbe2d7b2b5c803ebf8f3c87dcf958063
MAITRAEE MITRA <maitraee131@gmail.com>
2024-07-04 09:39:16 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
- 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return  {
- @type {import('next').NextConfig} 
+
+ module.exports = async (phase, { defaultConfig }) => {
+  
+  @type {import('next').NextConfig} 
 
 const nextConfig = {
         eslint: {
